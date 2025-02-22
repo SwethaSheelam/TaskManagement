c181babeb2e17e149cab3152f0a63a413d659e1b
sheelamswetha <sheelamswetha5@gmail.com>
2025-02-22 15:22:10 +0530

    Basic CRUD operations crated successfully

--- models/taskModel.js
+++ models/taskModel.js
@@ -7,7 +7,7 @@ const taskSchema = new mongoose.Schema({
   priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
   dueDate: { type: Date, required: true },
   status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending" },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User",}
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
 });
 
 module.exports = mongoose.model("Task", taskSchema);
